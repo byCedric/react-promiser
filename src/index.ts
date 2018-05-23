485cde686f559432de0ad23be65e61de01840d0b
Cedric van Putten <me@bycedric.com>
2018-05-23 17:23:14 +0200

    refactor: remove bulk promise api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from './utils/types';
 export { default as Promiser, PromiserProps, PromiserState } from './components/promiser';
-export { default as BulkPromiser, BulkPromiserProps, BulkPromiserState } from './components/bulk-promiser';
