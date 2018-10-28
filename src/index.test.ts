6041da766bb1042ab5bba2ea6daba5523933377f
Cedric van Putten <me@bycedric.com>
2018-10-28 02:36:43 +0200

    chore: restart the project and remove old code

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from 'react';
-import { Promiser } from './index';
-
-describe('index', () => {
-	it('exports Promiser component', () => {
-		expect(Promiser).toBeInstanceOf(Function);
-	});
-})
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './utils/types';
-export { default as Promiser, PromiserProps, PromiserState } from './components/promiser';
